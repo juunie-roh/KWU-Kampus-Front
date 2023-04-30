534fa117e75d307a221b83db40f14b1c9d727c98
Juun <blacklily7513@gmail.com>
2023-04-30 17:48:29 +0900

    modal > plane is replaced by text

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ import * as THREE from 'three';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 import { MapControls } from 'three/addons/controls/MapControls.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { FontLoader } from 'three/addons/loaders/FontLoader.js';
 
 let camera, controls, scene, renderer;
 const buildings = [];
@@ -43,7 +44,7 @@ function init() {
 
   // GLTF Loader
   const gltfLoader = new GLTFLoader();
-  loadModel( gltfLoader, './models/SaeBit.glb', new THREE.Vector3( 112, 0, -460 ), -106, 2 );
+  loadModel( gltfLoader, './models/SaeBit.glb', new THREE.Vector3( 112, 0, -460 ), 'SaeBit', -106, 2 );
 
   // world floor
   const planeSize = 2000;
@@ -139,13 +140,16 @@ fixedHelp.addEventListener( 'click', () => {
  * 
  * `loader` 를 사용해 `modelPath` 에 있는 모델을 불러옵니다.   
  * 모델의 위치, 회전시킬 각도, 크기 조정을 위한 스케일을 설정하여 `scene` 및 `buildings` 리스트에 추가하고, `createModal()` 에 `position` 을 전달합니다.
+ * 
+ * 모델만 불러오고, 모델의 position, angle, scale 을 설정하는 기능을 나눠야함.
  * @param { GLTFLoader } loader `GLTFLoader` used in this file.
  * @param { string } modelPath 
  * @param { THREE.Vector3 } position 
+ * @param { string } name 
  * @param { number } angle 
  * @param { number } scale 
  */
-async function loadModel ( loader, modelPath, position, angle, scale ) {
+async function loadModel ( loader, modelPath, position, name, angle, scale ) {
 
   await loader.load( modelPath, async ( gltf ) => {
 
@@ -154,7 +158,7 @@ async function loadModel ( loader, modelPath, position, angle, scale ) {
     model.rotateY( Math.PI / 180 * angle );
     model.scale.setScalar( scale );
     
-    createModal( model.position );
+    createModal( model.position, name );
     buildings.push( model );
     scene.add( model );
 
@@ -183,10 +187,11 @@ async function loadModel ( loader, modelPath, position, angle, scale ) {
 
 /**
  * 건물 이름 표시를 위한 3D 모달 생성 함수입니다.   
- * `position` 에 해당하는 위치에서 `THREE.Line` 과 `THREE.Mesh (plane)` 을 갖는 `THREE.Group` 을 생성합니다.
- * @param {THREE.Vector3} position position of a target model
+ * `position` 에 해당하는 위치에서 `THREE.Line` 과 `THREE.Mesh (text)` 을 갖는 `THREE.Group` 을 생성합니다.
+ * @param {THREE.Vector3} position position of the target model
+ * @param { string } name name of the target building
  */
-function createModal( position ) {
+function createModal( position, name ) {
   // Drawing Lines:
   const points = [];
   points.push( new THREE.Vector3( 0, 0, 0 ) );
@@ -201,17 +206,42 @@ function createModal( position ) {
 
   // Create Plane:
   // (Will be replaced by Text)
-  const plane = new THREE.Mesh(
-    new THREE.PlaneGeometry( 50, 50, 8, 8, ),
-    new THREE.MeshBasicMaterial( { color: 0x000000, side: THREE.FrontSide, transparent: true, opacity: 0.5 } )
-  );
-  plane.position.set( 50, 50, 50 );
-  plane.material.depthTest = false; // for renderOrder
+  // const plane = new THREE.Mesh(
+  //   new THREE.PlaneGeometry( 50, 50, 8, 8, ),
+  //   new THREE.MeshBasicMaterial( { color: 0x000000, side: THREE.FrontSide, transparent: true, opacity: 0.5 } )
+  // );
+  // plane.position.set( 50, 50, 50 );
+  // plane.material.depthTest = false; // for renderOrder
+
+  const loader = new FontLoader();
+  loader.load('./fonts/helvetiker_bold.typeface.json', function (font) {
+
+    const material = new THREE.MeshBasicMaterial({
+      color: 0x000000,
+      transparent: false,
+      side: THREE.FrontSide,
+    });
+
+    const message = name;
+    const shapes = font.generateShapes(message, 10);
+    const geometry = new THREE.ShapeGeometry(shapes);
+
+    geometry.computeBoundingBox();
+    const yMid = 0.5 * (geometry.boundingBox.max.y - geometry.boundingBox.min.y);
+    // geometry.translate(0, yMid, 0);
+
+    // make shape ( N.B. edge view not visible )
+    const text = new THREE.Mesh(geometry, material);
+    text.position.set( 50, 50, 50 );
+    text.material.depthTest = false; // for renderOrder
+    group.add(text);
+
+  }); //end load function
 
   // Create Group:
   const group = new THREE.Group();
   group.add( line );
-  group.add( plane );
+  // group.add( plane );
   group.position.copy( position );
   modals.push( group );
   group.renderOrder = 1; // renderOrder (z-index)
