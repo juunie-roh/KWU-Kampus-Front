ba842aa044993d4eaa1787fc518ce19e4000ec90
Juun <blacklily7513@gmail.com>
2023-05-20 13:43:33 +0900

    click event target change, from window to 'main'

--- js/main.js
+++ js/main.js
@@ -131,6 +131,7 @@ fixedHelp.addEventListener( 'click', () => {
 } );
 
 const subCategories = document.querySelectorAll( 'ul.sub-categories li a' );
+const main = document.querySelector( 'main' );
 
 ///////////////////////////////
 ///// THREE.js from here: /////
@@ -165,7 +166,7 @@ function init() {
   renderer = new THREE.WebGLRenderer( { antialias: true } );
   renderer.setPixelRatio( window.devicePixelRatio );
   renderer.setSize( width, height );
-  document.querySelector( 'main' ).appendChild( renderer.domElement ); // where to append
+  main.appendChild( renderer.domElement ); // where to append
 
   camera = new THREE.PerspectiveCamera( 60, width / height, 1, 700 );// 1000 );
   camera.position.set( 200, 100, 0 ); // ( 400, 200, 0 );
@@ -251,8 +252,8 @@ function init() {
   // scene.add( gridHelper );
 
   window.addEventListener( 'resize', onWindowResize );
-  window.addEventListener( 'pointermove', onPointerMove );
-  window.addEventListener( 'click', onClick );
+  main.addEventListener( 'pointermove', onPointerMove );
+  main.addEventListener( 'click', onClick );
   // window.addEventListener( 'dblclick', ( event ) => { // dev, 더블 클릭시 카메라의 위치에서 카메라 방향으로 
   //   let worldDirection = new THREE.Vector3;
   //   let worldPosition = new THREE.Vector3;
